861aafd4e122702fc5378c67bacf2b424150801e
wangshaojun <wangshaojun@baidu.com>
2013-12-22 17:49:05 +0800

    fefefefefefefe

--- gen/com/mucao/android/R.java
+++ gen/com/mucao/android/R.java
@@ -11,18 +11,41 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_menu_directions=0x7f020000;
+        public static final int ic_menu_gps=0x7f020001;
+        public static final int ic_menu_layers=0x7f020002;
+        public static final int ic_menu_layers_on=0x7f020003;
+        public static final int ic_menu_layers_pressed=0x7f020004;
+        public static final int ic_menu_more=0x7f020005;
+        public static final int ic_menu_mylocation=0x7f020006;
+        public static final int ic_menu_mylocation_on=0x7f020007;
+        public static final int ic_menu_mylocation_pressed=0x7f020008;
+        public static final int ic_menu_places=0x7f020009;
+        public static final int ic_menu_places_on=0x7f02000a;
+        public static final int ic_menu_places_pressed=0x7f02000b;
+        public static final int ic_menu_search=0x7f02000c;
+        public static final int icon=0x7f02000d;
+        public static final int search_toolbar_button=0x7f02000e;
     }
     public static final class id {
-        public static final int buttonZoomIn=0x7f050001;
-        public static final int buttonZoomOut=0x7f050002;
+        public static final int buttonZoomIn=0x7f050005;
+        public static final int buttonZoomOut=0x7f050006;
+        public static final int layers=0x7f050003;
         public static final int map=0x7f050000;
+        public static final int mylocation=0x7f050004;
+        public static final int places=0x7f050002;
+        public static final int search_edit=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int find=0x7f040004;
+        public static final int gpslogging=0x7f040003;
         public static final int hello=0x7f040000;
+        public static final int more=0x7f040006;
+        public static final int position=0x7f040002;
+        public static final int query=0x7f040005;
     }
 }
--- src/com/mucao/android/JMFoundation.java
+++ src/com/mucao/android/JMFoundation.java
@@ -1,7 +1,12 @@
 package com.mucao.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -26,8 +31,6 @@ public class JMFoundation extends Activity {
 	
 	private Button buttonZoomIn = null; 
 	private Button buttonZoomOut = null; 
-
-
 	
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -56,7 +59,8 @@ public class JMFoundation extends Activity {
 			public void onClick(View v) {
 				JMFoundation.this.map.zoomout(); 
 		 } 
-		 }); 
+		 });
+		
 
 		//Sets 'OnSingleTapListener' to 'MapView'
 		this.map.setOnSingleTapListener(new OnSingleTapListener() {
@@ -81,7 +85,83 @@ public class JMFoundation extends Activity {
 
 			}
 		});
-		
 
 	}
+	
+    //添加menu
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		MenuItem findItem = menu.add(0,0,0,R.string.find);
+		findItem.setIcon(R.drawable.ic_menu_search);
+		
+		MenuItem positionItem = menu.add(0,1,1,R.string.gpslogging);
+		positionItem.setIcon(R.drawable.ic_menu_gps);
+		
+		MenuItem queryItem = menu.add(0,2,2,R.string.query);
+		queryItem.setIcon(R.drawable.ic_menu_directions);
+		
+		SubMenu subMenu=menu.addSubMenu(0,3,3,R.string.more);
+		subMenu.setIcon(android.R.drawable.ic_menu_more);
+		
+	    subMenu.add(0,4,0,"GPS设置");
+	    subMenu.add(0,5,1,"系统帮助");
+	    subMenu.add(0,6,2,"版权声明");
+	    subMenu.add(0,7,3,"添加标注");
+	    
+	    //setMenuBackgroud();
+		return super.onCreateOptionsMenu(menu);
+	}
+    
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		
+		 switch(item.getItemId())
+		    {
+		      case 0:
+		    	  //搜索设置
+		         //SearchView search = new SearchView(this,this.mapView);
+		         ///search.ShowQueryWin();
+		         
+		        break;
+		      case 1:
+		    	  //GPS跟踪
+		    	    //Intent intent = new Intent(this, ControlTracking.class);
+		            //startActivity(intent);
+				
+		        break;
+		      case 2:
+		    	  //路线查询
+		    	    //Intent intent1 = new Intent(this, GoogleMapRoutes.class);
+		            //startActivity(intent1);
+		    	  
+		    	 // mapView.removeLocationOverlay(overlay);
+		          break;
+		      case 3:
+		          
+		          break;
+		      case 4:
+		    	  
+		    	  
+		          break;
+		      case 5:
+		    		
+		          break;
+		      case 6:
+		      {}
+		  		
+		          break;
+		      case 7:
+		    	 // double lat = 30.581429;//佳丽广场
+			     // double lng = 114.285877;
+			      double lat = 30.581429;//佳丽广场
+			      double lng = 114.285877;
+			      
+		    	  //mapView.addLabel(lng,lat,"佳丽广场",Html.fromHtml("佳丽广场"),R.drawable.poi_2);
+		          break;
+		    }
+		    return true;
+	}
+	
 }
